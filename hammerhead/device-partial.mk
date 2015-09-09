6c743efe70f189ad5850128d104d1184554a89e7
Jinghao Shi <jinghaos@buffalo.edu>
2015-09-09 17:58:51 -0400

    remove apns-conf.xml: copied in phonelab vendor blob

--- hammerhead/device-partial.mk
+++ hammerhead/device-partial.mk
@@ -60,8 +60,7 @@ PRODUCT_COPY_FILES := \
     vendor/lge/hammerhead/proprietary/libvss_nv_core.so:/system/vendor/lib/libvss_nv_core.so:lge \
     vendor/lge/hammerhead/proprietary/libvss_nv_idl.so:/system/vendor/lib/libvss_nv_idl.so:lge \
     vendor/lge/hammerhead/proprietary/libvss_nv_iface.so:/system/vendor/lib/libvss_nv_iface.so:lge \
-    vendor/lge/hammerhead/proprietary/vss_init:/system/vendor/bin/vss_init:lge \
-	vendor/lge/hammerhead/etc/apns-conf.xml:/system/etc/apns-conf.xml\
+    vendor/lge/hammerhead/proprietary/vss_init:/system/vendor/bin/vss_init:lge
 
 
 PRODUCT_PACKAGES +=\
